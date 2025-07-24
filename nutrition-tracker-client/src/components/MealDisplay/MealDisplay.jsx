fd46d6991fb12cdde3163d14be4b06b0be94b974
mlintelman <michaellintelman03@gmail.com>
2025-07-24 15:13:00 -0400

    Meal delete button and handler

--- nutrition-tracker-client/src/components/MealDisplay/MealDisplay.jsx
+++ nutrition-tracker-client/src/components/MealDisplay/MealDisplay.jsx
@@ -9,7 +9,9 @@ import 'react-datepicker/dist/react-datepicker.css'
 
 export default function MealDisplay() {
     const [meals, setMeals] = useState([])
+    const [mealItems, setMealItems] = useState([])
     const [selectedDate, setSelectedDate] = useState(new Date())
+    const [selectedMealId, setSelectedMealId] = useState(null)
 
     // When the page is loaded, fetch the meals
     useEffect(() => {
@@ -24,19 +26,6 @@ export default function MealDisplay() {
 
     const goToNextDay = () => {
         setSelectedDate(prev => addDays(prev, 1))
-    }
-
-    // Get all the user's meals based on their user id
-    const getUserMeals = async (user_id) => {
-        try {
-            const response = await fetch(`${API_BASE_URL}Meals/user/${user_id}`)
-            const data = await response.json()
-            //console.log(data)
-            setMeals(Array.isArray(data) ? data : [])
-        } catch (err) {
-            console.error("Failed to fetch meals:", err)
-            setMeals([])
-        }
     }
 
     // Get all the user's meals given a user id and date
@@ -55,6 +44,19 @@ export default function MealDisplay() {
         }
     }
 
+    // Get all the user's meal items given a meal id
+    const getMealItems = async (mealId) => {
+        try {
+            const response = await fetch(`${API_BASE_URL}MealItems/meal/${mealId}`)
+            const data = await response.json()
+            setMealItems(Array.isArray(data) ? data: [])
+            console.log(mealItems)
+        }
+        catch (err) {
+            console.error("Failed to fetch meal items:", err)
+            setMealItems([])
+        }
+    }
 
     // Take in a date time object and return a formatted date and time
     const formatDateTime = (dateTime) => {
@@ -63,38 +65,95 @@ export default function MealDisplay() {
         // Get the user's time zone
         const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
         // Return the formatted date converted to the time zone
-        return formatInTimeZone(utcDateTime, userTimeZone, "MM/dd/yyyy h:mm a")
+        return formatInTimeZone(utcDateTime, userTimeZone, "h:mm a")
     }
 
     const handleClickMeal = (mealId) => {
-        console.log(mealId)
+        if (mealId == selectedMealId) {
+            setSelectedMealId(-1)
+        }
+        else {
+        setSelectedMealId(mealId)
+        }
+        getMealItems(mealId)
     }
+
+    const handleDeleteMeal = async (mealId) => {
+        try {
+            // Send a delete request to the API
+            const response = await fetch(`${API_BASE_URL}Meals/${mealId}`, {
+                method: 'DELETE',
+            })
+            // If it goes through, update the meals list
+            if (response.ok) {
+                setMeals((prevMeals) => prevMeals.filter((meal) => meal.id !== mealId))
+                if (selectedMealId === mealId) {
+                    setSelectedMealId(null)
+                    setMealItems([])
+                }
+
+            } else {
+                console.error('Failed to delete meal.')
+            }
+        } catch (error) {
+            console.error('Error:', error)
+        }
+    }
+
+    useEffect(() => {
+        console.log(selectedMealId)
+    }, [selectedMealId])
 
 
     return(
         <div className={styles.mealDisplayContainer}>
-        <div className={styles.dateControls}>
-            <button onClick={goToPreviousDay}>←</button>
-            <DatePicker
-                selected={selectedDate}
-                onChange={date => setSelectedDate(date)}
-                dateFormat="MM/dd/yyyy"
-            />
-            <button onClick={goToNextDay}>→</button>
-        </div>
+            <div className={styles.dateControls}>
+                <button className={styles.dateControlsButton} onClick={goToPreviousDay}>←</button>
+                <DatePicker
+                    selected={selectedDate}
+                    onChange={date => setSelectedDate(date)}
+                    dateFormat="MM/dd/yyyy"
+                    className={styles.customDatePicker}
+                />
+                <button className={styles.dateControlsButton} onClick={goToNextDay}>→</button>
+            </div>
 
             {meals.length === 0 ? (
             <p>No meals logged yet.</p>
             ) : (
-                <ul>
-                    {meals.map((meal) => (
-                        <li 
+                <div className={styles.mealListContainer}>
+                    <ul className={styles.mealList}>
+                        {meals.map((meal) => (
+                            <li 
                             key={meal.id}
-                            onClick={() => handleClickMeal(meal.id)}>
-                            <strong>{meal.name}</strong>: {formatDateTime(meal.dateTime)}
-                        </li>
-                    ))}
-                </ul>
+                            >
+                                <strong>{meal.name}</strong>: {formatDateTime(meal.dateTime)} <button onClick={() => handleClickMeal(meal.id)}>{meal.id === selectedMealId ? (<p>↑</p>) : (<p>↓</p>)}</button><br/>
+                                {selectedMealId === meal.id && (
+                                    <ul className={styles.mealItemsList}>
+                                        {mealItems.map((mealItem) => (
+                                            <li
+                                                key={mealItem.id}>
+                                                <p>{mealItem.name}</p>
+                                                <ul className={styles.nutrientList}>
+                                                    <li>Calories: {mealItem.calories} kcal</li>
+                                                    <li>Protein: {mealItem.protein}g</li>
+                                                    <li>Carbs: {mealItem.carbs}g</li>
+                                                    <li>Fat: {mealItem.fat}g</li>
+                                                </ul>
+                                            </li>
+                                        ))}
+                                        
+                                        <span>
+                                            <button>Edit</button>
+                                            <button onClick={() => handleDeleteMeal(meal.id)}>Delete</button>
+                                        </span>
+                                    </ul>
+                                    
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             )}
         </div>
     )
